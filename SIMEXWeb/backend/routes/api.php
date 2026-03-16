d904e90f860c6a658e4a480abb60f2424d493858
ddelacortep <ddelacortep@gmail.com>
2026-03-16 17:12:56 +0100

    crud controller

--- SIMEXWeb/backend/routes/api.php
+++ SIMEXWeb/backend/routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\DatosMaestros;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('datos-maestros/{tabla}', [DatosMaestros::class, 'index']);
+Route::post('datos-maestros/{tabla}', [DatosMaestros::class, 'store']);
+Route::put('datos-maestros/{tabla}/{id}', [DatosMaestros::class, 'update']);
+Route::delete('datos-maestros/{tabla}/{id}', [DatosMaestros::class, 'destroy']);
