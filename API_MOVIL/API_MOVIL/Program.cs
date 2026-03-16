5d5be04a5fb6350a18f820515eaac4c9750b3446
Edith <gmirandab2425@politecnics.barcelona>
2026-03-16 17:49:55 +0100

    creacion de clave de seguridad para conexión con api laravel

--- API_MOVIL/API_MOVIL/Program.cs
+++ API_MOVIL/API_MOVIL/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// lectura de llave
+var secretKey = builder.Configuration["JwtConfig:Secret"];
+var keyBytes = Encoding.UTF8.GetBytes(secretKey);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+    options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true, // firma válida
+            IssuerSigningKey = new SymmetricSecurityKey(keyBytes), // clave secreta
+            ValidateIssuer = false,   
+            ValidateAudience = false,
+            ValidateLifetime = true, 
+            ClockSkew = TimeSpan.Zero
+        };
+    });
 
 var app = builder.Build();
 
@@ -18,7 +41,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseAuthentication(); // lee token
+app.UseAuthorization(); // lee permisos
 
 app.MapControllers();
 
