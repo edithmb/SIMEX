8bf521bb169cb8921544f1b062d0632c95caddd5
Edith <gmirandab2425@politecnics.barcelona>
2026-03-23 17:12:23 +0100

    configuraciones en Program, creacion del controlador de clientes

--- API_MOVIL/API_MOVIL/Program.cs
+++ API_MOVIL/API_MOVIL/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+using API_MOVIL.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<Simex06Context>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
