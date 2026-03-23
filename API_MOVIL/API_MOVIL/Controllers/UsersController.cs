939b6c3a41f54b4a7f04741d8228985d2f0d0ae6
Ledys2312 <lmaradiagas2425@politecnics.barcelona>
2026-03-23 17:22:08 +0100

    Creacion de user controller.

---
+++ API_MOVIL/API_MOVIL/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using API_MOVIL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_MOVIL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly Simex06Context _context;
+
+        public UsersController(Simex06Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+    }
+}
