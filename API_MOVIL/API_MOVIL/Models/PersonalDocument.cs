0ff5c79005161a295f75f387bbbcfe357406db08
Edith <gmirandab2425@politecnics.barcelona>
2026-03-23 15:57:21 +0100

    actualizacion del orm

---
+++ API_MOVIL/API_MOVIL/Models/PersonalDocument.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_MOVIL.Models;
+
+public partial class PersonalDocument
+{
+    public int Id { get; set; }
+
+    public int? PersonalDocumentTypeId { get; set; }
+
+    public string EntityType { get; set; } = null!;
+
+    public int EntityId { get; set; }
+
+    public string FileName { get; set; } = null!;
+
+    public string FilePath { get; set; } = null!;
+
+    public long? FileSizeBytes { get; set; }
+
+    public string? MimeType { get; set; }
+
+    public bool IsEncrypted { get; set; }
+
+    public string? EncryptionKey { get; set; }
+
+    public int UploadedBy { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual PersonalDocumentsType? PersonalDocumentType { get; set; }
+
+    public virtual User UploadedByNavigation { get; set; } = null!;
+}
