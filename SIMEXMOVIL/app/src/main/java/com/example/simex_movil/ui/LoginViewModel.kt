f5d89101fd867221b0afc0f8b3c1278bff354962
Ledys2312 <lmaradiagas2425@politecnics.barcelona>
2026-04-10 15:50:20 +0200

    Primer intento de conectar la api de laravel con android, y creacion de recursos necesarios

---
+++ SIMEXMOVIL/app/src/main/java/com/example/simex_movil/ui/LoginViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.simex_movil.ui
+
+import android.graphics.Mesh
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.simex_movil.network.LoginRequest
+import com.example.simex_movil.network.RetrofitClient
+import kotlinx.coroutines.launch
+
+//Estados de la pantalla al momento de iniciar sesion
+sealed class LoginState {
+    object Loading : LoginState()
+    data class Success(val token: String) : LoginState()
+    data class Error(val message: String) : LoginState()
+}
+
+class LoginViewModel : ViewModel(){
+
+    private val _loginState = MutableLiveData<LoginState>()
+    val loginState: LiveData<LoginState> = _loginState
+
+    //Se llama a la funcion cuando el usuario le de al boton de iniciar sesion
+    fun starLogin(email: String, password: String) {
+        _loginState.value = LoginState.Loading
+
+        viewModelScope.launch {
+            try {
+                val request = LoginRequest(email, password)
+
+                val response = RetrofitClient.apiService.login(request)
+
+                if(response.isSuccessful && response.body() != null){
+                    val token = response.body()!!.token
+                    _loginState.value = LoginState.Success(token)
+                } else {
+                    _loginState.value = LoginState.Error("Credenciales incorrectas")
+                }
+            } catch (e: Exception) {
+                _loginState.value = LoginState.Error("Error de conexión: Verifica tu red")
+            }
+        }
+
+
+    }
+
+}
