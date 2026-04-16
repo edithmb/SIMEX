9f3f35838ed7a67488c12a796cb3c44e3e9746a7
Edith <gmirandab2425@politecnics.barcelona>
2026-04-16 15:19:45 +0200

    cambio de estado en ooeraciones logisticas por parte del agente

--- SIMEXMOVIL/app/src/main/java/com/example/simex_movil/ui/Request.kt
+++ SIMEXMOVIL/app/src/main/java/com/example/simex_movil/ui/Request.kt
@@ -26,3 +26,16 @@ data class UpdateStatusResponse(
     val newStatus: String,
     val deliveredAt: String?
 )
+
+// info para .net para cambiar estado de operacion logistica
+data class UpdateLogisticsStatusRequest(
+    val status: String
+)
+
+// respuesta de la api
+data class UpdateStatusResponse(
+    val message: String,
+    val operationId: Int,
+    val newStatus: String,
+    val deliveredAt: String?
+)
